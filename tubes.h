3d6df71decd75797bda8922d4ac5d291ae7f6bef
ghifaryyy <ghifarywibisono1@gmail.com>
2025-11-25 15:05:55 +0700

    coba kode

--- tubes.h
+++ tubes.h
@@ -1,1 +1,13 @@
-cjguvvbv
+#ifndef TUBES_H
+#define TUBES_H
+
+#include <iostream>
+using namespace std;
+
+struct infotype_lagu{
+    string judul, artis, album, genre;
+    int tahun, durasi;
+}
+
+
+#endif
