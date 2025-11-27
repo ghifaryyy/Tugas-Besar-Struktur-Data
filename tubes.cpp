693337415cbdf636de5428f0ac59d78cc1662006
Neviyya <neviyamaulidya12@gmail.com>
2025-11-27 16:47:52 +0700

    nambahin isi procedure

---
+++ tubes.cpp
@@ -0,0 +1,93 @@
+#include "tubes.h"
+#include <iostream>
+using namespace std;
+
+void createUser_keongracun(list_User &U){
+    U.first = NULL;
+    U.last = NULL;
+}
+
+void createParent_keongracun(list_playlist &P){
+    P.first = NULL;
+    P.last = NULL;
+}
+
+void createChild_keongracun(list_lagu &L){
+    L.first = NULL;
+    L.last = NULL;
+}
+
+void createRelasi__keongracun(list_relasi &R){
+    R.first = NULL;
+    R.last = NULL;
+}
+
+bool isUserEmpty_keongracun(adr_User U){
+    if (U.first == NULL && U.last == NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isParentEmpty_keongracun(adr_playlist P){
+    if (P.first == NULL && P.last == NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isChildEmpty_keongracun(adr_lagu L){
+    if (L.first == NULL && L.last == NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isRelasiEmpty_keongracun(adr_relasi R){
+    if (R.first == NULL && R.last == NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+adr_User createNodeUser_keongracun(int id, string usn, string peran){
+    adr_User U = new elmlist_user;
+    U->info.id = id;
+    U->info.username = usn;
+    U->info.peran = peran;
+    U->next = NULL;
+    return U;
+}
+
+adr_playlist createNodeParent_keongracun(int id, string nama){
+    adr_playlist P = new elmlist_playlist;
+    P->info.id = id;
+    P->info.nama = nama;
+    P->next = NULL;
+    return P;
+}
+
+adr_lagu createNodeChild_keongracun(string judul, string album, string genre, int idlagu, int tahun, int durasi){
+    adr_lagu L = new elmlist_lagu;
+    L->info.judul = judul;
+    L->info.album = album;
+    L->info.genre = genre;
+    L->info.idlagu = idlagu;
+    L->info.tahun = tahun;
+    L->info.durasi = durasi;
+    L->next = NULL;
+    L->prev = NULL;
+    return L;
+}
+
+adr_relasi createNodeRelasi_keongracun(adr_playlist P, adr_lagu L){
+    adr_relasi R = new elmlist_relasi;
+    R->playlist = P;
+    R->lagu = L;
+    R->next = NULL;
+    return R;
+}
