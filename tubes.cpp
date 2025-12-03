bda7e1a585ef48c7859b90fd39e6285f3cc0a9cc
Neviyya <neviyamaulidya12@gmail.com>
2025-12-03 21:24:09 +0700

    merapikan code

--- tubes.cpp
+++ tubes.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 void createUser_keongracun(list_User &U){
-    U.first = NULL;
-    U.last = NULL;
+    U.first = nullptr;
+    U.last = nullptr;
 }
 
 void createParent_keongracun(list_playlist &P){
-    P.first = NULL;
-    P.last = NULL;
+    P.first = nullptr;
+    P.last = nullptr;
 }
 
 void createChild_keongracun(list_lagu &L){
-    L.first = NULL;
-    L.last = NULL;
+    L.first = nullptr;
+    L.last = nullptr;
 }
 
 void createRelasi__keongracun(list_relasi &R){
-    R.first = NULL;
-    R.last = NULL;
+    R.first = nullptr;
+    R.last = nullptr;
 }
 
 bool isUserEmpty_keongracun(adr_User U){
-    if (U.first == NULL && U.last == NULL){
+    if (U.first == nullptr && U.last == nullptr){
         return true;
     } else {
         return false;
@@ -31,7 +31,7 @@ bool isUserEmpty_keongracun(adr_User U){
 }
 
 bool isParentEmpty_keongracun(adr_playlist P){
-    if (P.first == NULL && P.last == NULL){
+    if (P.first == nullptr && P.last == nullptr){
         return true;
     } else {
         return false;
@@ -39,7 +39,7 @@ bool isParentEmpty_keongracun(adr_playlist P){
 }
 
 bool isChildEmpty_keongracun(adr_lagu L){
-    if (L.first == NULL && L.last == NULL){
+    if (L.first == nullptr && L.last == nullptr){
         return true;
     } else {
         return false;
@@ -47,7 +47,7 @@ bool isChildEmpty_keongracun(adr_lagu L){
 }
 
 bool isRelasiEmpty_keongracun(adr_relasi R){
-    if (R.first == NULL && R.last == NULL){
+    if (R.first == nullptr && R.last == nullptr){
         return true;
     } else {
         return false;
@@ -59,7 +59,7 @@ adr_User createNodeUser_keongracun(int id, string usn, string peran){
     U->info.id = id;
     U->info.username = usn;
     U->info.peran = peran;
-    U->next = NULL;
+    U->next = nullptr;
     return U;
 }
 
@@ -67,7 +67,7 @@ adr_playlist createNodeParent_keongracun(int id, string nama){
     adr_playlist P = new elmlist_playlist;
     P->info.id = id;
     P->info.nama = nama;
-    P->next = NULL;
+    P->next = nullptr;
     return P;
 }
 
@@ -79,8 +79,8 @@ adr_lagu createNodeChild_keongracun(string judul, string album, string genre, in
     L->info.idlagu = idlagu;
     L->info.tahun = tahun;
     L->info.durasi = durasi;
-    L->next = NULL;
-    L->prev = NULL;
+    L->next = nullptr;
+    L->prev = nullptr;
     return L;
 }
 
@@ -88,6 +88,6 @@ adr_relasi createNodeRelasi_keongracun(adr_playlist P, adr_lagu L){
     adr_relasi R = new elmlist_relasi;
     R->playlist = P;
     R->lagu = L;
-    R->next = NULL;
+    R->next = nullptr;
     return R;
 }
--- tubes.h
+++ tubes.h
@@ -9,90 +9,99 @@ typedef struct elmlist_lagu *adr_lagu;
 typedef struct elmlist_playlist *adr_playlist;
 typedef struct elmlist_relasi *adr_relasi;
 
-## info User
+// info User
 struct infotype_user{
     int id;
     string username;
     string role;
 };
-## Node User
+
+// List Parent
+struct list_playlist{
+    adr_playlist first;
+    adr_playlist last;
+};
+
+// Node User
 struct elmlist_user{
     infotype_user info;
     adr_User next;
-    adr_playlist playlist;
+    list_playlist Playlist;
 };
 
-## info Child
+// info Child
 struct infotype_lagu{
     string judul, album, genre;
     int id_lagu, tahun, durasi;
 };
 
-## Node Child
+// Node Child
 struct elmlist_lagu{
     infotype_lagu info;
     adr_lagu next;
     adr_lagu prev;
 };
 
-## info Parent
+// info Parent
 struct infotype_playlist{
     int id;
     string nama;
 };
 
-## Node Parent
+// Node Parent
 struct elmlist_playlist{
     infotype_playlist info;
     adr_playlist next;
+    adr_playlist prev;
+    adr_relasi firstRelasi;
 };
 
-## Node Relasi
+// Node Relasi
 struct elmlist_relasi{
     adr_playlist playlist;
     adr_lagu lagu;
     adr_relasi next;
+    adr_relasi prev;
 };
 
-## List User
-List User
+// List User
 struct list_User{
     adr_User first;
     adr_User last;
 };
 
-## List Child
+// List Child
 struct list_lagu{
     adr_lagu first;
     adr_lagu last;
 };
 
-List Parent
-struct list_playlist{
-    adr_playlist first;
-    adr_playlist last;
-};
-
-List relasi
+// List relasi
 struct list_relasi{
     adr_relasi first;
     adr_relasi last;
 };
 
+// ===== CREATE LIST =====
 void createUser_keongracun(list_User &U);
 void createParent_keongracun(list_playlist &P);
 void createChild_keongracun(list_lagu &L);
-void createRelasi__keongracun(list_relasi &R);
+void createRelasi_keongracun(list_relasi &R);
 
-bool isUserEmpty_keongracun(adr_User U);
-bool isParentEmpty_keongracun(adr_playlist P);
-bool isChildEmpty_keongracun(adr_lagu L);
-bool isRelasiEmpty_keongracun(adr_relasi R);
+// ===== CEK KOSONG =====
+bool isUserEmpty_keongracun(list_User U);
+bool isParentEmpty_keongracun(list_playlist P);
+bool isChildEmpty_keongracun(list_lagu L);
+bool isRelasiEmpty_keongracun(list_relasi R);
 
+// ===== CREATE NODE =====
 adr_User createNodeUser_keongracun(int id, string usn, string peran);
 adr_playlist createNodeParent_keongracun(int id, string nama);
 adr_lagu createNodeChild_keongracun(string judul, string album, string genre, int idlagu, int tahun, int durasi);
 adr_relasi createNodeRelasi_keongracun(adr_playlist P, adr_lagu L);
+
+void adminAddLagu_keongracun(list_lagu &L, adr_lagu song);
+void displayAllLagu_keongracun(list_lagu L);
 
 
 #endif
