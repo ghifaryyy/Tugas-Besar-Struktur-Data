cf924a92495db6b63244af3b1eb6ce6ddb8868b1
Neviyya <neviyamaulidya12@gmail.com>
2025-12-07 17:36:13 +0700

    header baru

--- tubes.h
+++ tubes.h
@@ -1,7 +1,10 @@
 #ifndef TUBES_H
 #define TUBES_H
 
+
 #include <iostream>
+#include <cstdlib>
+#include <iomanip>
 using namespace std;
 
 typedef struct elmlist_user *adr_User;
@@ -31,7 +34,7 @@ struct elmlist_user{
 
 // info Child
 struct infotype_lagu{
-    string judul, album, genre;
+    string nama,judul, album, genre;
     int id_lagu, tahun, durasi;
 };
 
@@ -48,12 +51,19 @@ struct infotype_playlist{
     string nama;
 };
 
+// List relasi
+struct list_relasi{
+    adr_relasi first;
+    adr_relasi last;
+};
+
 // Node Parent
 struct elmlist_playlist{
     infotype_playlist info;
     adr_playlist next;
     adr_playlist prev;
     adr_relasi firstRelasi;
+    list_relasi relasi;
 };
 
 // Node Relasi
@@ -76,11 +86,8 @@ struct list_lagu{
     adr_lagu last;
 };
 
-// List relasi
-struct list_relasi{
-    adr_relasi first;
-    adr_relasi last;
-};
+//Clear Screen
+void clearScreen();
 
 // ===== CREATE LIST =====
 void createUser_keongracun(list_User &U);
@@ -97,33 +104,32 @@ bool isRelasiEmpty_keongracun(list_relasi R);
 // ===== CREATE NODE =====
 adr_User createNodeUser_keongracun(int id, string usn, string peran);
 adr_playlist createNodeParent_keongracun(int id, string nama);
-adr_lagu createNodeChild_keongracun(string judul, string album, string genre, int idlagu, int tahun, int durasi);
+adr_lagu createNodeChild_keongracun(int idlagu, string nama, string judul, string album, string genre, int tahun, int durasi);
 adr_relasi createNodeRelasi_keongracun(adr_playlist P, adr_lagu L);
 
-// ===== ADMIN FUNCTION =====
+// ===== fitur admin =====
+void menuAdmin_keongracun(list_User &U,list_lagu &L);
 void adminAddLagu_keongracun(list_lagu &L, adr_lagu song);
 void displayAllLagu_keongracun(list_lagu L);
 adr_lagu searchIdLagu_keongracun(list_lagu L, int idLagu);
-void updateLagu_keongracun(adr_lagu &L, string new_judul, string new_album, string new_genre, int new_tahun, int new_durasi);
+void updateLagu_keongracun(adr_lagu L, string new_nama, string new_judul, string new_album, string new_genre, int new_tahun,
+                           int new_durasi);
+void adminDeleteLagu_keongracun(list_User &U, list_lagu &L, int id);
 void deleteLagu_keongracun(list_lagu &L, adr_lagu &song);
 
-
-// ===== USER FUNCTION =====
-void addUser_keongracun(list_User &U, adr_User user);
-adr_User findUserById_keongracun(list_User U, int id);
-void CreatePlaylistForUser_keongracun(list_User &U, adr_User user, adr_playlist P);
-
-
-// ===== PLAYLIST FUNCTION =====
-void addPlaylist_keongracun(list_playlist &P, adr_playlist playlist);
-void displayAllPlaylist_keongracun(list_playlist P);
-void deletePlaylist_keongracun(list_playlist &P, adr_playlist &playlist);
-
-
-// ===== RELATIONSHIP FUNCTION =====
+// ===== fitur user =====
+void addUser_keongracun(list_User &U, adr_User userBaru);
+void menuUser_keongracun(adr_User user, list_lagu &L);
+adr_User searchUser_keongracun(list_User U, string username);
+adr_playlist searchIdPlaylist(list_playlist P, int id);
 void addSongToPlaylist_keongracun(adr_playlist P, adr_lagu L);
+void deleteSongFromPlaylist_keongracun(adr_playlist P, int idLagu);
 void displaySongsInPlaylist_keongracun(adr_playlist P);
-void deleteSongFromPlaylist_keongracun(adr_playlist P, adr_lagu L);
+void userAddPlaylist_keongracun(adr_User user, int id, string nama);
+void DummyLagu_keongracun(list_lagu &L);
+void displayAllPlaylist(adr_User user);
 
+// RELASI
+void deleteRelasiLagu_keongracun(list_User &U, adr_lagu song);
 
 #endif
